242fbf56477bc56077cf86511084d0adf916145a
Mengsen <mengsen_wang@163.com>
2021-02-07 10:56:17 +0800

    cpp version

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2021-02-07 10:40:06
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2021-02-07 10:55:10
+ */
+
+// TODO
+// 开启线程库 -- 注意竟态
+// 寻找 inp -- 多文件处理
+// 执行分析
+// 得到结果
+// 清理垃圾
+
+#include <cstdio>
+#include <cstdlib>
+
+int main() {
+  printf("%d", system("dir"));
+  return 0;
+}
